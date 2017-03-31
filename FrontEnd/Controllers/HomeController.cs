270b92189bc49e621d5278f4062bd4c66c7840a0
Alex Fedotyev <Alex Fedotyev>
2017-03-30 21:00:37 -0700

    Add project files.

---
+++ FrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Azure Intro Lab";
+
+            Worker.BackgroundJob.Current.Schedule(GetUrl("Index"));
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "Azure Intro Lab";
+
+            ViewBag.Message = "Your application description page.";
+
+            Worker.BackgroundJob.Current.Schedule(GetUrl("About"));
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Azure Intro Lab";
+
+            ViewBag.Message = "Your contact page.";
+
+            Worker.BackgroundJob.Current.Schedule(GetUrl("Contact"));
+
+            return View();
+        }
+
+        private string GetUrl(string value)
+        {
+            string url = Request.Url.GetLeftPart(UriPartial.Authority) + Url.Content("~/api/TestApi/" + value);
+            return url;
+        }
+    }
+}
---
+++ FrontEnd/Worker/BackgroundJob.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Threading;
+
+namespace FrontEnd.Worker
+{
+    public class BackgroundJob
+    {
+        private static BackgroundJob instance = new BackgroundJob();
+
+        private ConcurrentQueue<string> queue;
+
+        private BackgroundJob()
+        {
+            queue = new ConcurrentQueue<string>();
+        }
+
+        public static BackgroundJob Current
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void Schedule(string value)
+        {
+            queue.Enqueue(value);
+        }
+
+        private Task worker = null;
+        private CancellationTokenSource token = null;
+
+        public void Start()
+        {
+            lock(this)
+            {
+                token = new CancellationTokenSource();
+                worker = Task.Run(async () => await RunLoop(), token.Token);
+            }
+        }
+
+        public void Stop()
+        {
+            lock(this)
+            {
+                if (token != null)
+                {
+                    token.Cancel();
+                    worker.Wait();
+
+                    token = null;
+                    worker = null;
+                }
+            }
+        }
+
+        private async Task RunLoop()
+        {
+            while(true)
+            {
+                if (token.IsCancellationRequested)
+                    break;
+
+                if (queue.IsEmpty)
+                {
+                    await Task.Delay(200);
+                    continue;
+                }
+
+                string value;
+                if(queue.TryDequeue(out value))
+                {
+                    try
+                    {
+                        DoWork(value);
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        private void DoWork(string value)
+        {
+            var c = new WebClient();
+            c.DownloadString(value);
+        }
+    }
+}
